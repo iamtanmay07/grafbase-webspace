494c03e4e7c6defdc0ffe6de2fb213d980be7d87
Tanmay <>
2024-03-31 02:30:04 +0530

    update on config

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,6 +1,7 @@
-import { g, config, auth } from '@grafbase/sdk';
+import { graph, config, auth } from '@grafbase/sdk';
 
-// @ts-ignore
+const g = graph.Standalone();
+
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
@@ -10,10 +11,9 @@ const User = g.model('User', {
   linkedinUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
 }).auth((rules) => {
-  rules.public().read()
-})
+  rules.public().read();
+});
 
-// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
@@ -23,20 +23,19 @@ const Project = g.model('Project', {
   category: g.string().search(),
   createdBy: g.relation(() => User),
 }).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
-})
+  rules.public().read();
+  rules.private().create().delete().update();
+});
 
 const jwt = auth.JWT({
   issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
-})
+  secret: g.env('NEXTAUTH_SECRET')
+});
 
 export default config({
-  schema: g,
+  graph: g,
   auth: {
     providers: [jwt],
-    rules: (rules) => rules.private()
+    rules: (rules) => rules.private(),
   },
-})
-
+});
